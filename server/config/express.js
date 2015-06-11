05584b6111b6a3ac9c5743f3e1bae20608c41dfc
xuancong246 <xuancong246@yahoo.com>
2015-06-11 23:41:15 +0700

    Modifying css and role

--- server/config/express.js
+++ server/config/express.js
@@ -1,22 +1,31 @@
 var logger = require('morgan'),
-  bodyParser = require('body-parser'),
-  cookieParser = require('cookie-parser'),
-  express = require('express'),
-  session = require('express-session'),
-  passport = require('passport');
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    express = require('express'),
+    session = require('express-session'),
+    passport = require('passport'),
+    stylus = require('stylus');
 
 module.exports = function(app, config) {
-  app.set('views', config.viewsPath);
-  app.set('view engine', 'jade');
-  app.use(logger('dev'));
-  app.use(cookieParser());
-  app.use(bodyParser());
-  app.use(bodyParser.urlencoded({
-      extended: true
-  }));
-  app.use(session({secret: 'financial unicorns'}));
-  app.use(passport.initialize());
-  app.use(passport.session());
+    function compile(str, path) {
+        return stylus(str).set('filename', path);
+    }
 
-  app.use(express.static(config.staticPath));
+    app.set('views', config.viewsPath);
+    app.set('view engine', 'jade');
+    app.use(logger('dev'));
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(session({secret: 'financial unicorns'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(stylus.middleware({
+        src: config.stylusSrcPath,
+        compile: compile
+    }));
+
+    app.use(express.static(config.staticPath));
 };
